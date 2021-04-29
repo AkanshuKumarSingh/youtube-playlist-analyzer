fff25d37e1f55a98c12fa41371b9b7e1886bbbf0
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-04-29 20:48:30 +0530

    added exception handling code

--- yt.js
+++ yt.js
@@ -5,61 +5,64 @@ let title;
 let views;
 
 (async function f() {
-    let browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        args: ["--start-maximized"],
-    });
-    let pagesArr = await browser.pages();
-    page = pagesArr[0];
-    await page.goto(url);
-    await page.waitForSelector("h1#title",{visible:true})
-    await page.waitForSelector("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer",{visible:true});
-    let obj = await page.evaluate(function () {
-        let allelements = document.querySelectorAll("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");
-        let noOfVideos = allelements[0].innerText;
-        let noOfViews = allelements[1].innerText;
-        let title = document.querySelector("h1#title").innerText;
-        let obj = {
-            nfVideos : noOfVideos,
-            nfViews : noOfViews,
-            title
-        };
-        return obj;
-    })
+    try {
+        let browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ["--start-maximized"],
+        });
+        let pagesArr = await browser.pages();
+        page = pagesArr[0];
+        await page.goto(url);
+        await page.waitForSelector("h1#title", { visible: true })
+        await page.waitForSelector("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer", { visible: true });
+        let obj = await page.evaluate(function () {
+            let allelements = document.querySelectorAll("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");
+            let noOfVideos = allelements[0].innerText;
+            let noOfViews = allelements[1].innerText;
+            let title = document.querySelector("h1#title").innerText;
+            let obj = {
+                nfVideos: noOfVideos,
+                nfViews: noOfViews,
+                title
+            };
+            return obj;
+        })
 
-    console.log(obj.title + " " + obj.nfVideos + " " + obj.nfViews);
-    
-    let noOfVideos = obj.nfVideos.split(" ")[0];
-    noOfVideos = Number(noOfVideos);
-    
-    let videoSelector = "#video-title";
-    let duration = 
-    "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
+        console.log(obj.title + " " + obj.nfVideos + " " + obj.nfViews);
 
-    await page.waitForSelector(videoSelector,{visible:true})
-    await page.waitForSelector(duration,{visible:true});
-    // let titleDurArr = await page.evaluate(getTitleNDuration,videoSelector,
-    //     duration);
-    // console.table(titleDurArr);
-    await page.evaluate(function () {
-        let durationElems = document.querySelectorAll("#video-title");
-        durationElems[durationElems.length - 1].scrollIntoView(true);
-        console.log("hello");
-    });
+        let noOfVideos = obj.nfVideos.split(" ")[0];
+        noOfVideos = Number(noOfVideos);
 
+        let videoSelector = "#video-title";
+        let duration =
+            "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
+
+        await page.waitForSelector(videoSelector, { visible: true })
+        await page.waitForSelector(duration, { visible: true });
+        // let titleDurArr = await page.evaluate(getTitleNDuration,videoSelector,
+        //     duration);
+        // console.table(titleDurArr);
+        await page.evaluate(function () {
+            let durationElems = document.querySelectorAll("#video-title");
+            durationElems[durationElems.length - 1].scrollIntoView(true);
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
 })();
 
-function getTitleNDuration(videoSelector,duration) {
-        let titleElementArr = document.querySelectorAll(videoSelector);
-        let duartionElementArr = document.querySelectorAll(duration);
-        let titleDurArr = [];
-        for (let i = 0; i < duartionElementArr.length; i++) {
-            let title = titleElementArr[i].innerText.trim();
-            let duration = duartionElementArr[i].innerText.trim()
-            titleDurArr.push({title,duration});
-        }
-        return titleDurArr;
+function getTitleNDuration(videoSelector, duration) {
+    let titleElementArr = document.querySelectorAll(videoSelector);
+    let duartionElementArr = document.querySelectorAll(duration);
+    let titleDurArr = [];
+    for (let i = 0; i < duartionElementArr.length; i++) {
+        let title = titleElementArr[i].innerText.trim();
+        let duration = duartionElementArr[i].innerText.trim()
+        titleDurArr.push({ title, duration });
+    }
+    return titleDurArr;
 }
 
 
