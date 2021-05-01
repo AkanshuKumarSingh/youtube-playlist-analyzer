f646f2432448ffc19a58829ab8fbc587f669ddd0
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-05-01 05:35:48 +0530

    youtube playlist analyzer completed

--- yt.js
+++ yt.js
@@ -3,6 +3,8 @@ let url = "https://www.youtube.com/playlist?list=PLzkuLC6Yvumv_Rd5apfPRWEcjf9b1J
 let page;
 let title;
 let views;
+let cVideos = 0;
+
 
 (async function f() {
     try {
@@ -17,7 +19,7 @@ let views;
         await page.waitForSelector("h1#title", { visible: true })
         await page.waitForSelector("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer", { visible: true });
         let obj = await page.evaluate(function () {
-            let allelements = document.querySelectorAll("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");
+            let allelements = document.querySelectorAll("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");            
             let noOfVideos = allelements[0].innerText;
             let noOfViews = allelements[1].innerText;
             let title = document.querySelector("h1#title").innerText;
@@ -34,24 +36,41 @@ let views;
         let noOfVideos = obj.nfVideos.split(" ")[0];
         noOfVideos = Number(noOfVideos);
 
+        let i = 0;
+        while ((noOfVideos - cVideos) > 100) {
+            await scrollDown(page);
+            console.log(i);
+            i++;
+        }
+
+        await waitTillHTMLRendered(page);
+        await scrollDown();
+        console.log(cVideos);
+
         let videoSelector = "#video-title";
         let duration =
             "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
 
         await page.waitForSelector(videoSelector, { visible: true })
         await page.waitForSelector(duration, { visible: true });
-        // let titleDurArr = await page.evaluate(getTitleNDuration,videoSelector,
-        //     duration);
-        // console.table(titleDurArr);
-        await page.evaluate(function () {
-            let durationElems = document.querySelectorAll("#video-title");
-            durationElems[durationElems.length - 1].scrollIntoView(true);
-        });
+        let titleDurArr = await page.evaluate(getTitleNDuration,videoSelector,
+            duration);
+        console.table(titleDurArr);
+        
 
     } catch (err) {
         console.log(err);
     }
 })();
+
+async function scrollDown() {
+    let length = await page.evaluate(function () {
+        let titleElems = document.querySelectorAll("#video-title");
+        titleElems[titleElems.length - 1].scrollIntoView(true);
+        return titleElems.length; 
+    })
+    cVideos = length;
+}
 
 function getTitleNDuration(videoSelector, duration) {
     let titleElementArr = document.querySelectorAll(videoSelector);
@@ -65,4 +84,33 @@ function getTitleNDuration(videoSelector, duration) {
     return titleDurArr;
 }
 
+async function waitTillHTMLRendered(page, timeout = 30000) {
+    const checkDurationMsecs = 1000;
+    const maxChecks = timeout / checkDurationMsecs;
+    let lastHTMLSize = 0;
+    let checkCounts = 1;
+    let countStableSizeIterations = 0;
+    const minStableSizeIterations = 3;
 
+    while (checkCounts++ <= maxChecks) {
+        let html = await page.content();
+        let currentHTMLSize = html.length;
+
+        let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+
+        console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+
+        if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize)
+            countStableSizeIterations++;
+        else
+            countStableSizeIterations = 0; //reset the counter
+
+        if (countStableSizeIterations >= minStableSizeIterations) {
+            console.log("Page rendered fully..");
+            break;
+        }
+
+        lastHTMLSize = currentHTMLSize;
+        await page.waitForTimeout(checkDurationMsecs);
+    }
+};
